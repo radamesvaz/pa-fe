c30f5206482ccd743ac3b8c9a62c5607775d4a13
Radames Vaz <rgvtovar@hotmail.com>
2020-03-30 12:44:12 -0400

    adding files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css';
+import SignIn from './components/SignIn';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hola!
+        <SignIn />
       </header>
     </div>
   );
