3636bb4ef628ae398bf98fe7eb713840a0473d05
Radames Vaz <rgvtovar@hotmail.com>
2020-04-01 13:19:08 -0400

    adding image

--- src/components/Agregar.js
+++ src/components/Agregar.js
@@ -1,31 +1,69 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Agregar extends Component {
 
+
+  state = {
+
+    file: null
+
+  }
+
+  handleFile(e){
+
+    let file = e.target.files[0];
+
+    this.setState({ file: file})
+
+  }
+
+  onSubmitAcc = (e) => {
+
+    let file = this.state.file;
+
+    let formdata = new FormData();
+
+    formdata.append("image", file);
+
+    axios({
+      url: 'http://localhost:3000/upload-images',
+      method: 'post',
+      headers: {
+        authorization: ''
+      },
+      data: formdata
+    })/*.then(response => response.json())
+    .then(user => {
+      if(user){
+        console.log(user);
+      }
+    })
+  */
+  }
+
     render(){
         return(
-            <div class="container">
-            <div class="row">
-              <div class="offset-md-3 col-md-6">
-                 <div class="form-group files">
+            <div className="container">
+            <div className="row">
+              <div className="offset-md-3 col-md-6">
+                 <div className="form-group files">
                   <label>Sube una imagen</label>
                   <input type="file" 
-                  class="form-control" 
+                  className="form-control" 
+                  name="file"
+                  onChange={(e) => this.handleFile(e)}
                  // multiple onChange={this.onChangeHandler}
     
                   />
                 </div>  
-               {/* <div class="form-group">
-                <ToastContainer />
-                <Progress max="100" color="success" value={this.state.loaded} >{Math.round(this.state.loaded,2) }%</Progress>
-         
-                </div> */}
                 
-              {/*  <button type="button" 
-                class="btn btn-success btn-block" 
-                onClick={this.onClickHandler}
-                >Upload</button> */}
-    
+
+                <button 
+                onClick={(e) => this.onSubmitAcc(e)}
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                type="button" 
+                >Agregar</button>
                 
     
             </div>
